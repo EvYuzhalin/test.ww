cf1d72dca4db4bc0123d75cd28c4e9dd09f66501
Evgeniy <ev.yuzhalin@gmail.com>
2022-02-18 13:38:20 +0500

    add code

---
+++ index.js
@@ -0,0 +1,45 @@
+// slider 
+
+$(function(){
+    $('.slider1').owlCarousel({
+        items: 1,
+        autoWidth: 1280,
+        dots: false,
+        nav: true,
+        margin: 0,
+        
+    });
+    $('.slider2').owlCarousel({
+        items: 1,
+        autoWidth: 1280,
+        dots: false,
+        nav: true,
+        margin: 0,
+        margin: 12
+    }
+        
+    );
+  });
+
+
+//   modal open 
+
+$(document).ready(function(){
+	/*Открыть модальное окно*/
+	$('.menu__butt').click(function(){
+		$('.menu__mobile').css({'display': 'block'});
+	});
+	/*Закрыть модальное окно*/
+	$('.close').click(function(){
+		$('.menu__mobile').css({'display': 'none'});
+	});
+
+
+	$('.top__button').click(function(){
+		$('.modal__block').css({'display': 'flex'});
+	});
+
+	$('.close_modal').click(function(){
+		$('.modal__block').css({'display': 'none'});
+	});
+});
