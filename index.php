8e67cf9988ff9da213e80f1edaa0828e71329f77
EvYuzhalin <ev.yuzhalin@gmail.com>
2022-02-24 17:53:24 +0500

    add php file

---
+++ index.php
@@ -0,0 +1,440 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>test.WW</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.css" /> 
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.0.47/jquery.fancybox.min.js"></script>
+    <script src="owlcarousel/owl.carousel.min.js"></script>
+    <script src="./js/index.js"></script>
+</head>
+<body class="test">
+  <div class="wrapper">
+    <div class="menu__mobile">
+        <div class="catalog__butt_mobile">
+            <button class="ct_butt_mobile" type="">Каталог</button>
+        </div>
+        <nav class="nav__menu_mobile">
+            <a href="#b1">Новинки</a>
+            <a href="#b2">Аксессуары</a>
+            <a href="#b3">Акции</a>
+            <a href="#b4">Sale</a>
+        </ul>
+        </nav>
+        <a class="close" href="#">Закрыть</a>
+    </div>
+    <div class="modal__block">
+        <div class="modal__search">
+            <span class="close_modal">Закрыть</span>
+            <h2 class="modal__title">Выберите город</h2>
+            <input class="modal__input" type="text" placeholder="Поиск"/>
+            <div class="modal__ul_box">
+                <ul class="mobal__ul">
+                    <li>Абакан</li>
+                    <li>Азов</li>
+                    <li>Александров</li>
+                    <li>Анапа</li>
+                    <li>Ангарск</li>
+                    <li>Анжеро-Судженск</li>
+                    <li>Апатиты</li>
+                    <li>Архангельск</li>
+                    <li>Астрахань</li>
+                    <li>Ачинск</li>
+                    <li>Балаково</li>
+                    <li>Балахна</li>
+                    <li>Балашиха</li>
+                    <li>Балашов</li>
+                    <li>Барнаул</li>
+                    <li>Батайск</li>
+                    <li>Белгород</li>   
+                </ul>
+                <ul class="mobal__ul">
+                    <li>Белебей</li>
+                    <li>Белово</li>
+                    <li>Белогорск (Амурская область)</li>
+                    <li>Белорецк</li>
+                    <li>Белореченск</li>
+                    <li>Бердск</li>
+                    <li>Березники</li>
+                    <li>Березовский (Свердловская область)</li>
+                    <li>Бийск</li>
+                    <li>Биробиджан</li>
+                    <li>Благовещенск (Амурская область)</li>
+                    <li>Бор</li>
+                    <li>Борисоглебск</li>
+                    <li>Боровичи</li>
+                    <li>Братск</li>
+                    <li>Брянск</li>
+                    <li>Бугульма</li>
+                    
+                </ul>
+                <ul class="mobal__ul">
+                    <li>Буденновск</li>
+                    <li>Бузулук</li>
+                    <li>Буйнакск</li>
+                    <li>Великие Луки</li>
+                    <li>Великий Новгород</li>
+                    <li>Верхняя Пышма</li>
+                    <li>Видное</li>
+                    <li>Владивосток</li>
+                    <li>Владикавказ</li>
+                    <li>Владимир</li>
+                    <li>Волгоград</li>
+                    <li>Волгодонск</li>
+                    <li>Волжск</li>
+                    <li>Волжский</li>
+                    <li>Вологда</li>
+                    <li>Вольск</li>
+                    <li>Воркута</li>
+                    
+                </ul>
+                <ul class="mobal__ul">
+                    <li>Воронеж</li>
+                    <li>Воскресенск</li>
+                    <li>Воткинск</li>
+                    <li>Всеволожск</li>
+                    <li>Выборг</li>
+                    <li>Выкса</li>
+                    <li>Вязьма</li>
+                    <li>Гатчина</li>
+                    <li>Геленджик</li>
+                    <li>Георгиевск</li>
+                    <li>Глазов</li>
+                    <li>Горно-Алтайск</li>
+                    <li>Грозный</li>
+                    <li>Губкин</li>
+                    <li>Гудермес</li>
+                    <li>Гуково</li>
+                    <li>Гусь-Хрустальный</li>
+                    
+                </ul>
+                <ul class="mobal__ul">
+                    <li>Воронеж</li>
+                    <li>Воскресенск</li>
+                    <li>Воткинск</li>
+                    <li>Всеволожск</li>
+                    <li>Выборг</li>
+                    <li>Выкса</li>
+                    <li>Вязьма</li>
+                    <li>Гатчина</li>
+                    <li>Геленджик</li>
+                    <li>Георгиевск</li>
+                    <li>Глазов</li>
+                    <li>Горно-Алтайск</li>
+                    <li>Грозный</li>
+                    <li>Губкин</li>
+                    <li>Гудермес</li>
+                    <li>Гуково</li>
+                    <li>Гусь-Хрустальный</li>
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="header__block_top">
+        <div class="top__button">
+            <span>Санкт - Петербург</span>
+            <div class="butt__down" type=""></div>
+        </div>
+        <div class="top__menu">
+            <a href="#">Доставка</a>
+            <a href="#">Отзывы</a>
+            <a href="#">Контакты</a>
+        </ul>
+        </div>
+    </div>
+    <div class="header">
+        <div class="header__block">
+
+            <div class="header__nav">
+                <div class="header__logo">
+                    <img src="./images/logo.png" alt="" class="logo">
+                </div>
+                <div class="catalog__butt">
+                    <button class="ct_butt" type="">Каталог</button>
+                </div>
+                <nav class="nav__menu">
+                    <a href="#b1">Новинки</a>
+                    <a href="#b2">Аксессуары</a>
+                    <a href="#b3">Акции</a>
+                    <a href="#b4">Sale</a>
+                </ul>
+                </nav>
+                <div class="button__box">
+                    <button class="search__butt"></button>
+                    <button class="car__butt">
+                        <span>10</span>
+                    </button>
+                    <button  class="menu__butt"></button>
+                </div>
+            </div> 
+        </div>
+    </div>
+
+      <div class="container">
+
+        <div class="banner__block">
+            <h1 class="banner__title">Лёгкий шопинг</h1>
+            <div class="banner__text_block">
+                <div class="text__block">
+                <span>c <span class="span__bold">10</span> до <span class="span__bold">13</span>ч.</span>
+                <span class="text__red">-10% на весь чек</span>
+                </div>
+                <button class="text__butt">Перейти в каталог</button>
+            </div>
+        </div>
+
+        <div id="b1" class="new__block">
+            <h2 class="new__title">Новинки</h2>
+            <div id="owl-carousel-1" class="owl-carousel slider1">
+
+                <div class="item">
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new1.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new2.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new3.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new4.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new5.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new6.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new7.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new8.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                </div>
+                <div class="item item_none">
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new5.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new6.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new7.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new8.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new1.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new2.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new3.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new4.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                </div>
+                
+            </div>
+            <div class="new__button">
+                <button class="new__button_black">Перейти в каталог</button>
+                <button class="new__button_white">Все новинки</button>
+            </div>
+        </div>
+
+        <div id="b4" class="sale__block">
+            <div class="sale"></div>
+            <h2 class="new__title">SALE</h2>
+            <div id="owl-carousel-2" class="owl-carousel slider2">
+
+                <div class="item">
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new1.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new2.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new3.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new4.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                </div>
+
+                <div class="item item_none">
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new1.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new2.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new3.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                    <a href="#" target="_blank" class="">
+                        <img src="images/new4.png" alt="">
+                        <span>Платье на бретелях из экокожи</span>
+                        <span class="span__red">2999 р.</span>
+                    </a>
+                </div>
+
+            </div>
+            <div class="new__button">
+                <button class="new__button_black sale_butt">Все товары со скидкой</button>
+            </div>
+        </div>
+
+        <div id="b3" class="stock__block">
+            <div class="stock__header">
+                <h2 class="new__title">Акции</h2>
+                <button class="stock__butt">Все акции</button>
+            </div>
+            <div class="stock__images">
+                <a data-fancybox href="./images/stock1.png"  data-caption="fox1"><img src="./images/stock1.png" /></a>
+                <a data-fancybox href="./images/stock2.png"  data-caption="fox2"><img src="./images/stock2.png" /></a>
+                <a data-fancybox href="./images/stock3.png"  data-caption="fox3"><img src="./images/stock3.png" /></a>
+                <a data-fancybox href="./images/stock4.png"  data-caption="fox3"><img src="./images/stock4.png" /></a>
+            </div>
+        </div>
+        
+        <div id="b3" class="news__block">
+            <div class="stock__header">
+                <h2 class="new__title">Новости</h2>
+                <button class="stock__butt news">Все новости</button>
+            </div>
+            <div class="new__box">
+                <a class="news__link" href="#">
+                    <span class="link__span">11.11.2022</span>
+                    <p class="link__text_bottom">
+                        <span class="link__text_white">Платье на бретелях из экокожи</span>
+                        <span class="link__text_green">Платье на бретелях из экокожи</span>
+                    </p>
+                </a>
+                <a class="news__link width_link" href="#">
+                    <span class="link__span">11.11.2022</span>
+                    <p class="link__text_bottom">
+                        <span class="link__text_white">Платье на бретелях из экокожи</span>
+                        <span class="link__text_green">Платье на бретелях из экокожи</span>
+                    </p>
+                </a>
+            </div>
+        </div>
+
+        <div class="about__block">
+            <div class="about__text_box">
+                <h2 class="about__title">О компании</h2>
+                <p>Магазин ЖЕНСКОЙ одежды. Основан в июле 2006 года. Основновное направление – быстрая повседневная мода. В коллекции всегда представлены все модные тендеции сезона, самые популярные вещи и новинки. Конечно в ассортименте есть и  всеми любимая классика. А так же отдельную нишу занимает и спортивный стиль.</p>
+                <p class="about">Наша коллекция обновляется каждую неделю. По этому у нас в магазине представлен ОГРОМНЫЙ АССОРТИМЕНТ.</p>
+                <button class="about__butt">Подробнее</button>
+            </div>
+            <div class="about__img_box">
+                <img src="./images/about2.png" alt="">
+            </div>
+            <img class="img__width" src="./images/about1.png" alt="">
+        </div>
+
+        <div class="ceo__block">
+            <h2 class="ceo__title">СЕО заголовок</h2>
+            <p class="ceo__text">Сложно сказать, почему акционеры крупнейших компаний ограничены исключительно образом мышления. Таким образом, начало повседневной работы по формированию позиции играет определяющее значение для распределения внутренних резервов и ресурсов. В частности, внедрение современных методик создаёт необходимость включения в производственный план целого ряда внеочередных мероприятий с учётом комплекса кластеризации усилий.</p>
+        </div>
+
+      </div>
+        
+      
+  </div>
+  <footer class="footer__block">
+    <div class="footer__menu">
+      <div class="footer__element">
+          <img src="./images/logo2.png" alt="">
+          <a href="#">Магазин ЖЕНСКОЙ одежды. Основан в июле 2006 года. Основновное направление – быстрая повседневная мода.</a>
+      </div>
+      <div class="footer__element">
+        <h3>Заголовок 1</h3>
+        <a href="#">Каталог</a>
+        <a href="#">Новинки</a>
+        <a href="#">Аксессуары</a>
+        <a href="#">Акции</a>
+        <a href="#">Sale</a>
+      </div>
+      <div class="footer__element">
+        <h3>Заголовок 2</h3>
+        <a href="#">Доставка</a>
+        <a href="#">Обмен и возврат</a>
+        <a href="#">таблица размеров</a>
+      </div>
+      <div class="footer__element">
+        <h3>Контакты</h3>
+        <a href="#">Г. Тюмень, ул. Республики, д.155а, 3 этаж, офис 33.</a>
+        <a href="tel:+7 969 8000 107">+7 969 8000 107</a>
+        <a href="#">с 10:00 до 20:00  без перерыва и выходных.</a>
+        <div class="footer_social">
+            <a class="social vk" href="#"></a>
+            <a class="social inst" href="#"></a>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer__line"></div>
+    <p class="footer__copyright">«MASK» show-room 2022  ©  Все права защищены</p>
+</footer>
+</body>
+</html>
+
